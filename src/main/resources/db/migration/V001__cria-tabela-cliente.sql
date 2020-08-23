70a0b30da68a2f45b930b479142c2af6acb2a99a
Franklin <franklinpereira98@hotmail.com>
2020-08-23 20:44:26 -0300

    Nada

---
+++ src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -0,0 +1,8 @@
+create table cliente (
+	id bigint not null auto_increment,
+    nome varchar (60),
+    email varchar(255),
+    telefone varchar(20),
+    
+    primary key(id)
+);
---
+++ src/test/java/com/franklin/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franklin.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
