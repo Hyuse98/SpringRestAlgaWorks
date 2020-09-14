e2a1f2bd4d4590f54a35e40bb6b017a0996ed476
Franklin <franklinpereira98@hotmail.com>
2020-09-14 07:29:01 -0300

    Esqueci

---
+++ src/main/java/com/franklin/springrest/domain/ValidationGroups.java
@@ -0,0 +1,9 @@
+package com.franklin.springrest.domain;
+
+public interface ValidationGroups {
+	
+	public interface ClienteId{
+		
+	}
+
+}
--- src/main/java/com/franklin/springrest/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/franklin/springrest/domain/service/GestaoOrdemServicoService.java
@@ -5,21 +5,28 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.franklin.springrest.domain.exception.ExceptionsBO;
+import com.franklin.springrest.domain.model.Cliente;
 import com.franklin.springrest.domain.model.OrdemServico;
 import com.franklin.springrest.domain.model.StatusOrdemServico;
+import com.franklin.springrest.domain.repository.ClienteRepository;
 import com.franklin.springrest.domain.repository.OrdemServicoRepository;
 
 @Service
 public class GestaoOrdemServicoService {
-	
+
 	@Autowired
 	private OrdemServicoRepository ordemServiceRepository;
-	
-	public OrdemServico criar (OrdemServico ordemServico) {
+	@Autowired
+	private ClienteRepository clienteServiceRepository;
+
+	public OrdemServico criar(OrdemServico ordemServico) {
+		Cliente cliente = clienteServiceRepository.findById(ordemServico.getCliente().getId()).orElseThrow(() -> new ExceptionsBO("Cliente Não Encontrado"));
+		ordemServico.setCliente(cliente);
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(LocalDateTime.now());
 		return ordemServiceRepository.save(ordemServico);
-		
+
 	}
 
 }
